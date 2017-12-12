c8285b37419eebe12e30072b6c9781a8ad4b9c10
Alice Robinson <valkrysa@gmail.com>
2017-12-11 22:09:12 -0800

    finished up

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework.Constraints;
+//using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public abstract class MovingObject : MonoBehaviour {
